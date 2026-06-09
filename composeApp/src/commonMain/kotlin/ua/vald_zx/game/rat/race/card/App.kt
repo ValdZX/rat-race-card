b3427ed91afbb5335baf30ede5460b9e113abb9e
Vladyslav Khimichenko <vald_zx@csltd.com.ua>
2026-06-09 22:01:09 +0300

    Revert mocks

--- composeApp/src/commonMain/kotlin/ua/vald_zx/game/rat/race/card/App.kt
+++ composeApp/src/commonMain/kotlin/ua/vald_zx/game/rat/race/card/App.kt
@@ -12,7 +12,6 @@ import app.lexilabs.basic.sound.SoundByte
 import app.lexilabs.basic.sound.play
 import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
-import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import io.github.alexzhirkevich.compottie.*
 import io.github.sudarshanmhasrup.localina.api.LocalinaApp
@@ -22,12 +21,9 @@ import kotlinx.serialization.Serializable
 import nl.marc_apps.tts.TextToSpeechInstance
 import nl.marc_apps.tts.experimental.ExperimentalVoiceApi
 import org.koin.compose.KoinApplication
-import org.koin.dsl.koinConfiguration
 import rat_race_card.composeapp.generated.resources.Res
 import ua.vald_zx.game.rat.race.card.di.baseModule
-import ua.vald_zx.game.rat.race.card.screen.SelectTypeScreen
 import ua.vald_zx.game.rat.race.card.theme.AppTheme
-import ua.vald_zx.game.rat.race.card.theme.LocalThemeIsDark
 
 var lottieDiceAnimations: Map<Int, LottieComposition> = emptyMap()
 
@@ -44,7 +40,7 @@ val appKStore: KStore<AppDataStorageBean>
 
 @Composable
 internal fun App() {
-    KoinApplication(configuration = koinConfiguration(declaration = { modules(baseModule) }), content = {
+    KoinApplication(application = { modules(baseModule) }) {
         val lottieCache = LocalLottieCache.current
         LaunchedEffect(Unit) {
             lottieDiceAnimations = (1..6).associate { side ->
@@ -56,46 +52,39 @@ internal fun App() {
                 }
             }
         }
-        Navigator(SelectTypeScreen()) {
-            LocalinaApp {
-                AppTheme {
-                    var isDarkTheme by LocalThemeIsDark.current
-                    LaunchedEffect(Unit) {
-                        Napier.base(DebugAntilog())
-                        val storage = appKStore.get()
-                        val systemIsDark = storage?.theme ?: isDarkTheme
-                        isDarkTheme = systemIsDark
+//        Navigator(SelectTypeScreen()) { navigator ->
+//            LocalinaApp {
+//                AppTheme {
+//                    CurrentScreen()
+//                }
+//            }
+//        }
+
+        AppTheme {
+            val raceRate2store = koinInject<RatRace2CardStore>()
+            var kStoreLoaded by remember { mutableStateOf(false) }
+            if (kStoreLoaded) {
+                val raceRate2State by raceRate2store.observeState().collectAsState()
+                val hasProfession = raceRate2State.playerCard.profession.isNotEmpty()
+                val startScreen = if (hasProfession) RaceRate2Screen() else PersonCard2Screen()
+                Navigator(startScreen) {
+                    LocalinaApp {
+                        AppTheme {
+                            CurrentScreen()
+                        }
                     }
-                    CurrentScreen()
+                }
+            } else {
+                LaunchedEffect(Unit) {
+                    val state2 = runCatching { raceRate2KStore.get() }.getOrNull()
+                    if (state2 != null) {
+                        raceRate2store.dispatch(RatRace2CardAction.LoadState(state2))
+                    }
+                    kStoreLoaded = true
                 }
             }
         }
-
-        //        AppTheme {
-        //            val raceRate2store = koinInject<RatRace2CardStore>()
-        //            var kStoreLoaded by remember { mutableStateOf(false) }
-        //            if (kStoreLoaded) {
-        //                val raceRate2State by raceRate2store.observeState().collectAsState()
-        //                val hasProfession = raceRate2State.playerCard.profession.isNotEmpty()
-        //                val startScreen = if (hasProfession) RaceRate2Screen() else PersonCard2Screen()
-        //                Navigator(startScreen) {
-        //                    LocalinaApp {
-        //                        AppTheme {
-        //                            CurrentScreen()
-        //                        }
-        //                    }
-        //                }
-        //            } else {
-        //                LaunchedEffect(Unit) {
-        //                    val state2 = runCatching { raceRate2KStore.get() }.getOrNull()
-        //                    if (state2 != null) {
-        //                        raceRate2store.dispatch(RatRace2CardAction.LoadState(state2))
-        //                    }
-        //                    kStoreLoaded = true
-        //                }
-        //            }
-        //        }
-    })
+    }
 }
 
 @OptIn(ExperimentalBasicSound::class)
